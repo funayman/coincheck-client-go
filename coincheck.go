824048cd2a1d2ae4fb10bf5f7f0135628e8f7081
Dave Derderian <funayman@gmail.com>
2017-03-24 11:59:03 +0900

    create Ticker func
    set custom JSONUnmarshaler
    create custom Ticker type
    setup basic Ticker function

--- coincheck.go
+++ coincheck.go
@@ -5,6 +5,7 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -12,13 +13,14 @@ import (
 	"time"
 )
 
-type coinCheck struct {
-	apiKey    string
-	apiSecret string
+type CoinCheck struct {
+	apiKey     string
+	apiSecret  string
+	httpClient http.Client
 }
 
-func New(key, secret string) *coinCheck {
-	return &coinCheck{key, secret}
+func New(key, secret string) *CoinCheck {
+	return &CoinCheck{key, secret, http.Client{}}
 }
 
 func createSignature(nonce int64, url, secret string) string {
@@ -32,9 +34,53 @@ func createRequest(method, url string, data url.Values, key, secret string) (req
 	nonce := time.Now().UnixNano()
 
 	req, _ = http.NewRequest(method, url, bytes.NewBufferString(data.Encode()))
-
 	req.Header.Add("ACCESS-KEY", key)
 	req.Header.Add("ACCESS-NONCE", strconv.FormatInt(nonce, 10))
 	req.Header.Add("ACCESS-SIGNATURE", createSignature(nonce, url, secret))
+
 	return
+}
+
+//Public Api
+type Ticker struct {
+	Last      int       `json:"last"`
+	Bid       int       `json:"bid"`
+	Ask       int       `json:"ask"`
+	High      int       `json:"high"`
+	Low       int       `json:"low"`
+	Volume    float64   `json:"volume"`
+	Timestamp time.Time `json:"timestamp"`
+	Raw       []byte
+}
+
+func (t *Ticker) UnmarshalJSON(b []byte) (err error) {
+	type alias Ticker
+	tmp := &struct {
+		Timestamp int64 `json:"timestamp"`
+		*Ticker
+	}{
+		Ticker: (*Ticker)(t),
+	}
+
+	if err = json.Unmarshal(b, tmp); err != nil {
+		return err
+	}
+
+	t.Timestamp = time.Unix(tmp.Timestamp, 0)
+	t.Raw = b
+	return nil
+}
+
+func (client *CoinCheck) Ticker() (t Ticker, err error) {
+	url := "https://coincheck.com/api/ticker"
+	req := createRequest("GET", url, nil, client.apiKey, client.apiSecret)
+	resp, err := client.httpClient.Do(req)
+	if err != nil {
+		return t, err
+	}
+
+	if err = json.NewDecoder(resp.Body).Decode(&t); err != nil {
+		return t, err
+	}
+	return t, nil
 }
